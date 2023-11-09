1d56e293cacc2613a1b7c91c1cbe3ef0ccec7041
Tyler <tyler.cheswick@carvana.com>
2023-11-09 14:03:14 -0700

    added timer buttons

--- src/components/Form/Timer.tsx
+++ src/components/Form/Timer.tsx
@@ -2,8 +2,35 @@ import styled from "styled-components";
 
 const Container = styled.div`
   background-color: #010206;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
   height: 100%;
   width: 360px;
+`;
+
+const Button = styled.button`
+  border-radius: 99999px;
+  font-family: "Formula1";
+  font-size: 20px;
+  margin-bottom: 16px;
+`;
+
+const RedButton = styled(Button)`
+  border: 2px solid #aa0c0b;
+
+  &:hover {
+    background-color: #8b0a09;
+  }
+`;
+
+const GreenButton = styled(Button)`
+  background-color: #11fd01;
+  color: #010206;
+
+  &:hover {
+    background-color: #0bce00;
+  }
 `;
 
 const Time = styled.div`
@@ -14,7 +41,9 @@ const Time = styled.div`
 const Timer = () => {
   return (
     <Container>
-      <Time>00:00:00</Time>;
+      <Time>00:00:00</Time>
+      <GreenButton>Start</GreenButton>
+      <RedButton>Stop</RedButton>
     </Container>
   );
 };
