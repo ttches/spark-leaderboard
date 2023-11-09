262790074304ba7dd4250faa57caa517b3bee1be
Tyler <tyler.cheswick@carvana.com>
2023-11-09 13:45:50 -0700

    added height 100% to body, html

--- src/components/Form/Timer.tsx
+++ src/components/Form/Timer.tsx
@@ -1,11 +1,22 @@
 import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: #010206;
+  height: 100%;
+  width: 360px;
+`;
+
 const Time = styled.div`
   font-family: "Digital";
+  font-size: 95px;
 `;
 
 const Timer = () => {
-  return <Time>00:00:00</Time>;
+  return (
+    <Container>
+      <Time>00:00:00</Time>;
+    </Container>
+  );
 };
 
 export default Timer;
